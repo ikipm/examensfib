831494a4a3861004006cdfc34ac3be260b40a612
Iker Pérez <ikipm0@gmail.com>
2025-01-16 16:03:20 +0100

    Fix: Minor issue

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { use, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
